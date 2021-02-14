a2614b602139cdb427df706eb7505ca7be77088e
naeun <sne12345@naver.com>
2021-02-15 00:48:38 +0900

    first commit

---
+++ 02. CNN(1)/visual.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.utils import to_categorical
+
+def Visulaize(histories, key='loss'):
+    for name, history in histories:
+        val = plt.plot(history.epoch, history.history['val_'+key],
+                   '--', label=name.title()+' Test')
+        plt.plot(history.epoch, history.history[key], color=val[0].get_color(),
+             label=name.title()+' Train')
+
+    plt.xlabel('Epochs')
+    plt.ylabel(key.replace('_',' ').title())
+    plt.legend()
+    plt.xlim([0,max(history.epoch)])
+    plt.savefig("plot.png")
+#     elice_utils.send_image("plot.png")
